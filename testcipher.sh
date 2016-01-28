1f38275117ffc1c174a0abaaa2bbfa642e047473
elakamarcus <n0m4d@hotmail.com>
2016-01-28 16:03:34 +0800

    Create testcipher.sh
    ./testcipher.sh server port -- this will attempt to connect to a server:port using openssl to verify supported ciphers.

---
+++ testcipher.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+#prepare
+SERVER=$1
+PORT=$2
+DELAY=1
+ciphers=$(openssl ciphers 'ALL:eNULL' | sed -e 's/:/ /g')
+#end prepare
+#function
+function run {
+  for cipher in ${ciphers[@]}
+  do
+    result=$(echo -n | openssl s_client -cipher "$cipher" -connect $SERVER:$PORT 2>&1)
+    if [[ "$result" =~ ":error" ]]; then
+      error=$(echo -n $result | cut -d":" -f6)
+    else
+      if [[ "$result" =~ "Cipher is ${cipher}" || "$result" =~ "Cipher    :"  ]]; then
+        echo "[+]" $cipher
+      else
+        echo "[-]" $result
+      fi
+    fi
+  sleep $DELAY
+  done
+}
+#end function
+echo $SERVER:$PORT
+
+#check parameters
+if [[ -z "$1" || -z "$2" ]]; then
+  echo need server and port.
+  exit 1
+else
+  run
+fi
+done
