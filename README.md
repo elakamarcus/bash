809bb0d9ba228fc4e6ea4feab7f1744f03006036
elakamarcus <mlauren@hotmail.com>
2017-11-23 21:28:29 +0800

    refresh

--- README.md
+++ README.md
@@ -2,16 +2,30 @@
 bash baby!
 
 _backdoor.sh_
+
 - POC
+
+_genwordlist.sh_
+- generate a wordlit from URL, using cewl..
+```zsh
+./gen_wordlist.sh url
+```
 
 _greappalologs.sh_
 - cut out some fields from bluecoat url logs
 
 _nmap-http-enum.sh_
-- becuase lazy
+- becuase lazy. run all http enum scripts with nmap
+```zsh
+./nmap-http-enum.sh ip port
+```
+
 
 _testcipher.sh_
 - script to test supported tls ciphers. supply IP PORT.
+```zsh
+./testcipher.sh 127.0.0.1 443
+```
 
 _testmore.sh_
 - i forgot what this' about.......
--- genwordlist.sh
+++ genwordlist.sh
@@ -4,9 +4,9 @@ now=$(date +'%Y%m%d')
 useragent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
 
 function run () {
-	echo [+] Using URL $urls
+	echo [+] Using URL $url
 	echo [+] Using UA: "$useragent"
-	cewl -w temp.$now -e --email_file email.$now -u "$useragent" $urls
+	cewl -w temp.$now -e --email_file email.$now -u "$useragent" $url
 	echo [-] no error checking, sorry.
 	echo [!] tidy up...
 	tr '[:upper:]' '[:lower:]' < temp.$now > temp.lower.$now
@@ -14,12 +14,13 @@ function run () {
 	rm temp.$now temp.lower.$now
 	echo [+] Check the files:
 	echo [+] wordlist: wordlist.$now, email: email.$now
+	echo [!] Hint: Use with Korelogic rules and john
 
 }
 if [[ "$1" =~ "http://" || "$1" =~ "https://" || "$1" =~ "www" ]]; then
-	urls=$1
+	url=$1
 	run
 elif [[ -z "$1" ]]; then
-	echo Need URL...duh
+	echo Need URL... http(s)://... www...
 	exit 1
 fi
